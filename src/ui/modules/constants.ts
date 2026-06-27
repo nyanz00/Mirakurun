f58fcbd495a488bacc06945d1c2b484b70420168
nyanz00 <coco_nyamunyamu081@icloud.com>
2026-06-28 02:09:32 +0900

    Merge upstream 4.1.3

--- src/ui/modules/constants.ts
+++ src/ui/modules/constants.ts
@@ -15,7 +15,7 @@
 */
 import { IconName } from "@blueprintjs/core";
 
-export const forkVersion = "4.1.2-nyanz1.6";
+export const forkVersion = "4.1.3-nyanz1.0";
 export const forkRepositoryUrl = "https://github.com/nyanz00/Mirakurun";
 
 export const channelTypeMap = {
